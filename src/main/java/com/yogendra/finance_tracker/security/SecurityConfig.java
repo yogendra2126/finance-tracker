fe788667b2583877e36b06e45e5d5d097398a937
Yogendra <yogendrabaghel2002@gmail.com>
2025-06-20 13:26:24 +0530

    Update SecurityConfig

--- src/main/java/com/yogendra/finance_tracker/security/SecurityConfig.java
+++ src/main/java/com/yogendra/finance_tracker/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(false); // Use true only if needed
