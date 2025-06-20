09dcbc0d80942202e797c56b18ed64ab03cb6338
Yogendra <yogendrabaghel2002@gmail.com>
2025-06-20 13:48:37 +0530

    Update Configuration

--- src/main/java/com/yogendra/finance_tracker/config/OpenAPIConfig.java
+++ src/main/java/com/yogendra/finance_tracker/config/OpenAPIConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
@@ -33,6 +36,9 @@ public class OpenAPIConfig {
                                         .scheme("bearer")
                                         .bearerFormat("JWT")
                         )
-                );
+                )
+                .servers(List.of(
+                        new Server().url("https://finance-tracker-production-631d.up.railway.app")
+                ));
     }
 }
